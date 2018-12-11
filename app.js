ddaebe4c1ad4b2188cef3f38dc9d1470541efbe7
4bhishekKasam <konnurabhishek@gmail.com>
2018-12-11 21:01:04 +0530

    connected to database

--- app.js
+++ app.js
@@ -1,25 +1,42 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const taskController = require("./controllers/TaskController");
+const morgan = require("morgan");
+const MongoClient = require("mongodb").MongoClient;
+const cors = require("cors");
+const mongoose = require("mongoose");
+//const taskController = require("./controllers/TaskController");
 //const categoryController = require("./controllers/CategoryController");
 //const entryController = require("./controllers/EntryController");
 
-require("./config/db");
+const dbURI =
+  "mongodb://abhi_madmax:abhi190394@cluster0-shard-00-00-1xohs.mongodb.net:27017,cluster0-shard-00-01-1xohs.mongodb.net:27017,cluster0-shard-00-02-1xohs.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
 
 const app = express();
-
-const port = process.env.PORT || 3301;
+const port = parseInt(process.env.PORT || 8080);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-//task routes
-app
-  .route("/tasks")
-  .get(taskController.listTasks)
-  .post(taskController.createTask);
+const options = {
+  reconnectTries: Number.MAX_VALUE,
+  poolSize: 10
+};
 
-//-----------
+mongoose
+  .connect(
+    dbURI,
+    options
+  )
+  .then(
+    () => {
+      console.log("Database connection established!");
+    },
+    err => {
+      console.log("Error connecting Database instance due to: ", err);
+    }
+  );
+
+require("./models/task");
 
 app.listen(port, () => {
-  console.log("connected on localhost port :" + port);
+  console.log("connected to localhost port:" + port);
 });
